e7f310388a6a938fbc4ccef9e909ec70a1190f12
Yeon Lee <yeonlee@Yeons-MacBook-Air.local>
2019-07-10 09:52:45 -0400

    added more functions

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 var tbody = document.querySelector('#table tbody');
 var dataset = [];
+var stopFlag = false;
+var opnedColum = 0;
 
 document.querySelector('#exec').addEventListener('click', function(){
     // 내부를 먼저 초기화
     tbody.innerHTML = '';
     dataset = [];
+    document.querySelector('#result').textContent = '';
+    stopFlag = false;
+    opnedColum = 0;
+
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
@@ -29,19 +35,26 @@ document.querySelector('#exec').addEventListener('click', function(){
         dataset.push(arr);
 
         for(var j = 0; j < hor; j +=1){  
-           arr.push(1);
+           arr.push(0);
            var td = document.createElement('td');
+
            td.addEventListener('contextmenu', function(e){
                e.preventDefault();
+                if(stopFlag){
+                    return;
+                }
+
                var parentTr = e.currentTarget.parentNode;
                var parenttbody = e.currentTarget.parentNode.parentNode;
                var colum = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
                var row = Array.prototype.indexOf.call(parenttbody.children, parentTr);
 
                if(e.currentTarget.textContent === '' || e.currentTarget.textContent==='X' ){
-                e.currentTarget.textContent = '!';
+                e.currentTarget.textContent = '!';            
+
                }else if(e.currentTarget.textContent ==='!'){
                 e.currentTarget.textContent = '?';
+
                }else if(e.currentTarget.textContent ==='?'){
                 if(dataset[row][colum] === 1){
                     e.currentTarget.textContent = '';
@@ -54,16 +67,28 @@ document.querySelector('#exec').addEventListener('click', function(){
            });
            td.addEventListener('click', function(e){
                // 클릭했을때 주변 지뢰 개수
+            if(stopFlag) {
+                return;
+            }
             var parentTr = e.currentTarget.parentNode;
             var parenttbody = e.currentTarget.parentNode.parentNode;
             var colum = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
             var row = Array.prototype.indexOf.call(parenttbody.children, parentTr);
+            
+            console.log(dataset, row, colum);
+            if(dataset[row][colum] === 1){
+                return; // 이미 열린 칸
+            }
+
 
             e.currentTarget.classList.add('opened');
-
+            opnedColum +=1;
             if(dataset[row][colum] === 'X'){
                 e.currentTarget.textContent = '펑!';
+                document.querySelector('#result').textContent = 'Failed!';
+                stopFlag = true;
             } else {
+                dataset[row][colum] = 1;
                 var around = [dataset[row][colum-1],        dataset[row][colum+1]];
                 if(dataset[row-1]){
                     around = around.concat(dataset[row-1][colum-1], dataset[row-1][colum], dataset[row-1][colum+1]);
@@ -77,9 +102,10 @@ document.querySelector('#exec').addEventListener('click', function(){
                     return v === 'X';
                 }).length;   
 
-                e.currentTarget.textContent = numberAroundMine;
-
-                if(numberAroundMine === 0){
+                // false value = '', 0, NaN, null, undefined, false
+                e.currentTarget.textContent = numberAroundMine || '';
+                dataset[row][colum] = 1;
+                if(numberAroundMine === 0){ 
                     // 주변 8칸 동시 오픈
                     console.log('주변을 엽니다');
                     var aroundColum = [];
@@ -104,9 +130,20 @@ document.querySelector('#exec').addEventListener('click', function(){
                         ]);
                     }
                     aroundColum.filter((v) => !!v).forEach(function(nextColum){
+                    var parentTr = nextColum.parentNode;
+                    var parenttbody = nextColum.parentNode.parentNode;
+                    var nextColumCol = Array.prototype.indexOf.call(parentTr.children, nextColum);
+                     var nextColumRow = Array.prototype.indexOf.call(parenttbody.children, parentTr);
+                       if(dataset[nextColumRow][nextColumCol] !== 1 ){
                         nextColum.click();
+                       }                     
                     });
                 }                   
+            }
+            console.log(opnedColum, hor * ver - mine);
+            if(opnedColum === hor * ver - mine ){
+                stopFlag = true;
+                document.querySelector('#result').textContent = 'You won!';
             }
            });
            tr.appendChild(td);
